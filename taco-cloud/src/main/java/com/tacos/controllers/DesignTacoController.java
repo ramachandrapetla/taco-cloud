24761c10e2960a726241ca9ceb0ca18759940dd1
ramachandrapetla <ramachandra1996@gmail.com>
2023-06-10 22:51:16 -0400

    Design web page

--- taco-cloud/src/main/java/com/tacos/controllers/DesignTacoController.java
+++ taco-cloud/src/main/java/com/tacos/controllers/DesignTacoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -62,5 +63,12 @@ public class DesignTacoController {
 	@GetMapping
 	public String showDesignForm() {
 		return "design";
+	}
+	
+	@PostMapping
+	public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+		tacoOrder.addTaco(taco);
+		log.info("processing Taco: {}", taco);
+		return "redirect:/orders/current";
 	}
 }
